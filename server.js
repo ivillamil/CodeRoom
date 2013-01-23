a33021eee0e1092fd03dcd8ece93e7c6bcad7773
Iván Israel Villamil <tiroldesign@gmail.com>
2013-01-22 19:35:48 -0600

    dependencies included

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var io = require('socket.io').listen(8000);
 
 io.sockets.on('connection', function (socket) {
    
-   socket.on('css', function(data){
-      socket.broadcast.emit('css',{ css: data });
-   });
+	socket.on('css', function(data){
+		socket.broadcast.emit('css',{ css: data });
+	});
 
-   socket.on('html', function(data){
-      socket.broadcast.emit('html',{ html: data });
-   });
+	socket.on('html', function(data){
+		socket.broadcast.emit('html',{ html: data });
+	});
+
+	socket.on('js', function(data){
+		socket.broadcast.emit('js',{ html: data });
+	});
 
 });
